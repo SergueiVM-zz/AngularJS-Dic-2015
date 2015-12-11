4b9913782af4d54760e9c05189d8e666282ee260
Alberto Casero <alberto@kasfactory.net>
2015-12-11 12:15:56 +0100

    Modificamos el control del menu de navegación en base a escuchar eventos locationChangeSuccess y redirigimos a /movies al pulsar en el enlace de MovieDbClone

--- scripts/controllers/MenuController.js
+++ scripts/controllers/MenuController.js
@@ -1,32 +1,43 @@
-angular.module("MovieDbApp").controller("MenuController", ["$scope", function($scope){
+angular.module("MovieDbApp").controller("MenuController",
+    ["$scope", "$location", function($scope, $location){
 
-    $scope.activeItem = '';
+        $scope.activeItem = $location.path();
 
-    // actualiza el valor activeItem del scope
-    $scope.setActiveItem = function(activeItemId) {
-        this.activeItem = activeItemId;
-        this.$emit("ChangePageTitle", this.getPageTitle(activeItemId));
-    };
+        // escuchamos cuando cambiar la URL ($locationChangeSuccess)
+        $scope.$on("$locationChangeSuccess", function(){
+            $scope.setActiveItem($location.path());
+        });
 
-    // devuelve la clase CSS a utilizar en función del itemId
-    $scope.classForItem = function(itemId) {
-        if (itemId == this.activeItem) {
-            return 'active';
-        } else {
-            return '';
+        // actualiza el valor activeItem del scope
+        $scope.setActiveItem = function(activeItemId) {
+            this.activeItem = activeItemId.split("/")[1] || "";
+            if (this.activeItem == "") {
+                $location.url("/movies");
+            } else {
+                this.$emit("ChangePageTitle", this.getPageTitle(this.activeItem));
+            }
+        };
+
+        // devuelve la clase CSS a utilizar en función del itemId
+        $scope.classForItem = function(itemId) {
+            if (itemId == this.activeItem) {
+                return 'active';
+            } else {
+                return '';
+            }
+        };
+
+        // devuelve el título de página deseado en función del activeItemId pasado
+        $scope.getPageTitle = function(activeItemId) {
+            switch (activeItemId) {
+                case "movies":
+                    return "Movies list";
+                case "series":
+                    return "Series list";
+                case "people":
+                    return "People list";
+            }
         }
-    };
 
-    // devuelve el título de página deseado en función del activeItemId pasado
-    $scope.getPageTitle = function(activeItemId) {
-        switch (activeItemId) {
-            case "movies":
-                return "Movies list";
-            case "series":
-                return "Series list";
-            case "people":
-                return "People list";
-        }
-    }
-
-}]);
+    }]
+);
